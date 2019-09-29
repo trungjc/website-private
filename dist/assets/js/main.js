9e5e73a5ea87cc599b45a5224e428cb27fbb9cd5
trung nguyen <trungnguyenthanh70@gmail.com>
2019-09-29 18:31:44 +0700

    update css

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,40 +1,12 @@
 (function ($) {
   //doc ready
   $(function () {
-    var demo;
-    var inteval = 1;
-    $('.step').on('inview', function (event, isInView) {
-      var Thiz = $(this);
-      var section = Thiz.closest('.tpl-block');
-      var child = Thiz.children().length;
-
-      if (isInView) {
-        // element is now visible in the viewport
-        $(this).addClass('inview');
-        demo = setInterval(function () {
-          if (inteval <= child) {
-            var i = inteval++;
-            section.find('.img').removeClass('active');
-            section.find('.img:nth-child(' + i + ')').addClass('active');
-            section.find('.step li').removeClass('active');
-            section.find('.step li:nth-child(' + i + ')').addClass('active');
-
-            if (i == child) {
-              inteval = 1;
-            }
-          } else {
-            clearInterval(demo);
-          }
-        }, 2000);
-      } else {
-        if (demo) {
-          clearInterval(demo);
-        }
-      }
-    });
-    $('[data-fancybox]').fancybox({// caption : function( instance, item ) {
-      //     // return $(this).parent().find('.card-text').html();
-      // }
+    $('.navbar-nav-scroll li a').click(function (e) {
+      e.preventDefault();
+      var element = $(this).attr('href');
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $(element).offset().top
+      }, 500);
     });
   });
   $(window).on('load', function () {//after window loaded code here
