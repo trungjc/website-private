4ae9506a52a17e14fc779859ee5c15651b1116cf
trung nguyen <trungnguyenthanh70@gmail.com>
2019-09-29 18:12:54 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Learning and Development frontend build
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. 
+
+## Installation
+
+Quickly describe how to install project and how to get it running
+
+1. Install Node dependencies
+
+    Download and install node js
+    10.16.2 LTS
+    Recommended For Most Users
+    https://nodejs.org/en/
+
+    Using cmd line to run, also you can using Git Bash if you want
+
+        npm install
+
+2. After installing the latest version of Node.js, install with:
+    if these package exist, you don't need install again
+        $ npm install gulp-cli -g
+        $ npm install pug-cli -g
+
+## Usage
+
+### Development
+
+    Try with this cmd
+
+        $ gulp serve
+
+### Build src code to html
+    
+    You can check in "dist" folder
+
+        $ npm run build
+
+## Support & Ownership
+
+    Feel free to ask [Vu Ninh Hiep]vuninhhiep@gmail.com if you need some support when there are any questions left or if you need some support.
---
+++ dist/assets/js/main.js
@@ -0,0 +1,42 @@
+(function ($) {
+  //doc ready
+  $(function () {
+    var demo;
+    var inteval = 1;
+    $('.step').on('inview', function (event, isInView) {
+      var Thiz = $(this);
+      var section = Thiz.closest('.tpl-block');
+      var child = Thiz.children().length;
+
+      if (isInView) {
+        // element is now visible in the viewport
+        $(this).addClass('inview');
+        demo = setInterval(function () {
+          if (inteval <= child) {
+            var i = inteval++;
+            section.find('.img').removeClass('active');
+            section.find('.img:nth-child(' + i + ')').addClass('active');
+            section.find('.step li').removeClass('active');
+            section.find('.step li:nth-child(' + i + ')').addClass('active');
+
+            if (i == child) {
+              inteval = 1;
+            }
+          } else {
+            clearInterval(demo);
+          }
+        }, 2000);
+      } else {
+        if (demo) {
+          clearInterval(demo);
+        }
+      }
+    });
+    $('[data-fancybox]').fancybox({// caption : function( instance, item ) {
+      //     // return $(this).parent().find('.card-text').html();
+      // }
+    });
+  });
+  $(window).on('load', function () {//after window loaded code here
+  });
+})(jQuery);
